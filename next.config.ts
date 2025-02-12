72abe236c24dcc2e83688f5ddb5f41eaff576b82
Chamidu-Maduwantha <chamidumaduwntha2001@gmail.com>
2025-02-13 00:28:31 +0530

    portfolio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output:"export",
   /* config options here */
+  distDir: "dist",
+
+
 };
 
 export default nextConfig;
